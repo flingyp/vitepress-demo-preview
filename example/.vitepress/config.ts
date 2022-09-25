44b1401a136a07b56eb1c755d838171b3cdd467c
flingyp <1213563369@qq.com>
2022-09-25 17:16:13 +0800

    fix: handling online file 404

--- example/.vitepress/config.ts
+++ example/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 import { componentPreview, containerPreview } from '@vitepress-demo-preview/plugin'
 
 export default defineConfig({
+  base: '/vitepress-demo-preview',
   title: 'vitepress-demo-preview',
   description: 'Just playing around.',
   markdown: {
